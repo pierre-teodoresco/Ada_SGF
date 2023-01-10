4b8505f1e49e9b0c36bddec08d2ac9dca321d979
pierre-teodoresco <p.teodoresco@gmail.com>
2023-01-10 19:33:28 +0100

    debut sfg

--- src/sgf/p_sgf.ads
+++ src/sgf/p_sgf.ads
@@ -1,3 +1,17 @@
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
+
 package P_SGF is 
 
+    -- DF: le type Dossier Fichier
+    type DF_Type is (Dossier, Fichier);
+    type DF_Perm is (Lecture, Ecriture, Execution);
+    type DF is record
+        Nom: Unbounded_String;
+        Type: DF_Type;
+        Perm: DF_Perm;
+        Taille: Integer;
+    end record;
+
+    -- Arbre de Dossier Fichier
 end P_SGF;
