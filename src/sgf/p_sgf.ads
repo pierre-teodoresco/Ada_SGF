35cb6ac9470c2b1538522e472bdc0ec72564a728
Pierre Teodoresco <p.teodoresco@gmail.com>
2023-01-11 14:28:50 +0100

    structure sgf

--- src/sgf/p_sgf.ads
+++ src/sgf/p_sgf.ads
@@ -1,9 +1,20 @@
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with P_Arbre;
 
 
 package P_SGF is 
 
     -- DF: le type Dossier Fichier
+    type DF is private;
+
+    -- Arbre de Dossier Fichier
+    package Arbre_DF is 
+        new P_Arbre(Type_Element => DF);
+    use Arbre_DF;
+
+    
+
+private
     type DF_Type is (Dossier, Fichier);
     type DF_Perm is (Lecture, Ecriture, Execution);
     type DF is record
@@ -12,6 +23,4 @@ package P_SGF is
         Perm: DF_Perm;
         Taille: Integer;
     end record;
-
-    -- Arbre de Dossier Fichier
 end P_SGF;
