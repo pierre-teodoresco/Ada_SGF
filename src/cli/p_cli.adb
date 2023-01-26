6a613bfc5e265001db4237d26222ce4d03ca7ae0
Pierre Teodoresco <p.teodoresco@gmail.com>
2023-01-26 18:43:22 +0100

    h

--- src/cli/p_cli.adb
+++ src/cli/p_cli.adb
@@ -3,8 +3,23 @@ with Ada.Text_IO; use Ada.Text_IO;
 package body P_Cli is
 
     procedure Run(F_input: in Unbounded_String) is
+        System: SGF := P_SGF.Creer;
+        input: Liste_String := Separer_commande(F_input);
+        cmd: Unbounded_String := Get_cmd(input);
+        args: Liste_String := Get_args(input);
+        opt: Unbounded_String := Get_opt(input);
+        the_cmd: Commande;
     begin
-        null;
+        loop            
+            the_cmd.Nom := Commandes'Value(To_String(cmd));
+            the_cmd.Args := args;
+            the_cmd.Option := Options'Value(To_String(opt));
+            
+            Lancer(F_sgf => System, F_cmd => the_cmd);
+        exit when To_String(cmd) = "exit";
+        end loop;
+    exception
+        when CONSTRAINT_ERROR => Put_Line("La commande ou les options sont incorrectes.");
     end Run;
 
     procedure Run_test(F_input: in Unbounded_String) is
